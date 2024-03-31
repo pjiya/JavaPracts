c69791e8dbcb9bc8cfa6e8a690d205abc0962681
pjiya <115789648+pjiya@users.noreply.github.com>
2024-03-31 21:37:59 +0530

    Update Exp8_Fibonnaci.java

--- Exp8_Fibonnaci.java
+++ Exp8_Fibonnaci.java
@@ -20,7 +20,7 @@ public class Exp8_Fibonnaci {
                 int n3 = n1 + n2;
                 n1 = n2;
                 n2 = n3;
-                System.out.println(n1);;
+                System.out.println(n1);
                 count += 1;
                 
             }
