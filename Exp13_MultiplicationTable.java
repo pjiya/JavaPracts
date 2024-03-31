5d173109eb19df94c1ebcb18439ec998b79b57c7
pjiya <115789648+pjiya@users.noreply.github.com>
2024-03-31 21:55:18 +0530

    Update Exp13_MultiplicationTable.java

--- Exp13_MultiplicationTable.java
+++ Exp13_MultiplicationTable.java
@@ -8,7 +8,6 @@ public class Exp13_MultiplicationTable {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number for which you want the Multiplicaton Table: ");
         int a = sc.nextInt();
-        System.out.println("\n");
         for (int i=0;i<=10;i++){
             int b=a*i;
             System.out.println(a +" X "+ i +" = " + b);
